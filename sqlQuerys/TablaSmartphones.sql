2489dd037ecf2e79d8907ac778866cb80728fcf0
unknown <oscarortiz016ags@gmail.com>
2024-03-10 20:05:08 -0600

    more querys

--- sqlQuerys/TablaSmartphones.sql
+++ sqlQuerys/TablaSmartphones.sql
@@ -1,6 +1,24 @@
 use resguardoMEXQv3
 select * from equipos
 
+drop table equipos
+
+create table equipos (
+id int identity(1,1) Primary key,
+codigo varchar(255) not null,
+nomina varchar(255) not null,
+nombre varchar(255) not null,
+sucursal varchar(255) not null,
+area varchar(255) not null,
+marca varchar(255) not null,
+modelo varchar(255) not null,
+no_serie varchar(255) not null,
+fecha varchar(255) not null,
+no_factura varchar(255) not null,
+proveedor varchar(255) not null,
+estado varchar(255) not null,
+);
+
 Create table smartphones (
 	id int identity(1,1) Primary key,
 	codigo varchar(255) not null,
